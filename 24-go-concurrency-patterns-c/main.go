801c5c3b324a768be3f2f1b99af805de765a6727
kcsamm <kcsamm11@gmail.com>
2022-11-28 23:06:26 +0100

    Updated 'Go Concurrency Patterns C'

--- 24-go-concurrency-patterns-c/main.go
+++ 24-go-concurrency-patterns-c/main.go
@@ -4,9 +4,37 @@ import (
 	"time"
 )
 
+// func main() {
+// 	count("sheep")
+// 	count("fish")
+// }
+
+
+// func main() {
+// 	go count("sheep")
+// 	count("fish")
+// }
+
+
+// func main() {
+// 	go count("sheep")
+// 	go count("fish")
+// }
+
+
 func main() {
 	go count("sheep")
-	count("fish")
+	go count("fish")
+
+	// time.Sleep(time.Second * 2)
+	// fmt.Scanln()
+}
+
+func main() {
+	go count("sheep")
+	go count("fish")
+
+	fmt.Scanln()
 }
 
 func count(animal string) {
