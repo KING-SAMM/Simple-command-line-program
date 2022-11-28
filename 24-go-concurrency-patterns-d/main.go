dd08833e1d2066a83063df803f968dcd96625720
kcsamm <kcsamm11@gmail.com>
2022-11-28 23:18:32 +0100

    Added 'Go Concurrency Patterns D'

---
+++ 24-go-concurrency-patterns-d/main.go
@@ -0,0 +1,25 @@
+package main
+import (
+	"fmt"
+	"time"
+	"sync"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	go func() {
+		count("sheep")
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func count(animal string) {
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i, animal)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
